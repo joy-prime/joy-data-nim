8760884cace9fa7bb0879f448d47ef00423219c7
deansher <deansherthompson@gmail.com>
2018-12-03 11:34:39 -0500

    Project scaffold.

---
+++ joydata.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "deansher"
+description   = "modeling data as data in Nim"
+license       = "MIT"
+srcDir        = "src"
+
+
+# Dependencies
+
+requires "nim >= 0.19.0"
---
+++ src/joydata/submodule.nim
@@ -0,0 +1,7 @@
+# This is just an example to get you started. Users of your library will
+# import this file by writing ``import joydata/submodule``. Feel free to rename or
+# remove this file altogether. You may create additional modules alongside
+# this file as required.
+
+var
+  foo*: int
