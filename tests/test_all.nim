8b7bcba0c5b956988730f6887e7a8d80bcaf822e
deansher <deansherthompson@gmail.com>
2018-12-22 14:33:35 -0500

    Added desired idioms as tests that don't compile.

--- tests/test_all.nim
+++ tests/test_all.nim
@@ -1,13 +1,33 @@
-import macros
-import unittest
+import macros, unittest, options
 import joy
+
+dump:
+  name: string
+  age: int
+  parents: array[0..1, Option[string]]
 
 const name* = (qualifiedName: "joy¦test_all¦name")
 field(age, int)
 
-#data(Person, name, age)
+data(Person, name)
+data(PersonWithAge, name, age)
+data(Something)
 
-test "construct a data value and read fields":
-  #let fred = make(Person, name = "Fred", age = 12)
-  dumpTree:
-    assertEquals(fred./age, 12)
+test "construct data and read fields":
+  let fred = Person(name: "Fred", age: 12)
+  check:
+      fred./name == "Fred"
+      fred?/age = some(12)
+
+  let jack = PersonWithAge(name: "Jack", age: 15)
+  check:
+      jack./name == "Jack"
+      jack./age == 15
+
+  let jill = Something(name: "Jill", age: 13)
+  when compiles(jill./name == "Jill"):
+      checkpoint("That shouldn't have compiled because name is not a defined field of jill")
+      fail()
+  
+  check(jill.?name == some("Jill"),
+        jill.?age == none(int))
